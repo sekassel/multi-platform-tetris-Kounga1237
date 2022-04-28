d761e5f5b05e610b725fee5e3b485e2ebf17da31
Kounga1237 <tonykylek@gmail.com>
2022-04-28 14:16:39 +0200

    puzzle kyle

--- projects/tetris/src/app/app.module.ts
+++ projects/tetris/src/app/app.module.ts
@@ -13,7 +13,13 @@ import { AppComponent } from './app.component';
   imports: [
     GameEngineLibModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
