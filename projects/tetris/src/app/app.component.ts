c3ff1801420c390b0fd87dc41f4f667bdf5f8863
Kounga1237 <tonykylek@gmail.com>
2022-04-28 13:32:51 +0200

    kylepuzzle

---
+++ projects/tetris/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { GameEngineLibService } from '../../../game-engine-lib/src/public-api';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tetris';
+constructor(private engineService: GameEngineLibService) {
+    // console.info(engineService.testing);
+  }
+}
---
+++ projects/tetris/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+/*import {GameEngineLibModule} from "../../../game-engine-lib/src/lib/game-engine-lib.module";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+imports: [BrowserModule, GameEngineLibModule, CoreModule];
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    GameEngineLibModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+  
+})
+export class AppModule { }*/
